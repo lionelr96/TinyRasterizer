c9b955023a30eb6d3d0b72a7769c94b90369c6d9
LIONEL ROBERT <lionelr@umich.edu>
2022-08-14 12:39:09 -0400

    basic line drawing

--- TinyRasterizer/main.cpp
+++ TinyRasterizer/main.cpp
@@ -1,21 +1,32 @@
+#include <iostream>
 #include "tgaimage.h"
 
 const TGAColor white = TGAColor (255, 255, 255, 255);
 const TGAColor red = TGAColor (255, 0, 0, 255);
 
-void line (int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
-	for (float t = 0.0; t < 1.0; t += 0.01) {
-		int x = x0 + (x1 - x0) * t;
-		int y = y0 + (y1 - y0) * t;
+void draw_line (int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+	// make sure x0 < x1
+	if (x0 > x1) {
+		std::swap (x0, x1);
+		std::swap (y0, y1);
+	}
+	float a = (static_cast<float>(y1) - y0) / (x1 - x0);
+	float y = y0;
+
+	for (int x = x0; x <= x1; ++x) {
 		image.set (x, y, color);
+		y += a;
 	}
 }
 
 int main (int argc, char** argv)
 {
-	TGAImage image (100, 100, TGAImage::RGB);
-	image.set (52, 42, red);
+	TGAImage image (500, 500, TGAImage::RGB);
+
+	//line (13, 20, 80, 40, image, white);
+	draw_line (-50, -200, 60, 240, image, white);
 	image.flip_vertically ();
+
 	image.write_tga_file ("output.tga");
 	return 0;
 }
