a7bc7bab6c75a3cf708072ef0903f2c3201c4b72
LIONEL ROBERT <lionelr@umich.edu>
2022-08-14 12:45:56 -0400

    vertical and horizontal line support

--- TinyRasterizer/main.cpp
+++ TinyRasterizer/main.cpp
@@ -5,25 +5,48 @@ const TGAColor white = TGAColor (255, 255, 255, 255);
 const TGAColor red = TGAColor (255, 0, 0, 255);
 
 void draw_line (int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
-	// make sure x0 < x1
-	if (x0 > x1) {
-		std::swap (x0, x1);
-		std::swap (y0, y1);
-	}
-	float a = (static_cast<float>(y1) - y0) / (x1 - x0);
-	float y = y0;
+	float dx = x1 - x0;
+	float dy = y1 - y0;
 
-	for (int x = x0; x <= x1; ++x) {
-		image.set (x, y, color);
-		y += a;
+	if (std::abs (dx) > std::abs (dy)) {
+		// line is somewhat horizontal
+		// make sure x0 < x1
+		if (x0 > x1) {
+			std::swap (x0, x1);
+			std::swap (y0, y1);
+		}
+
+		float a = dy / dx;
+		float y = y0;
+
+		for (int x = x0; x <= x1; ++x) {
+			image.set (x, y, color);
+			y += a;
+		}
 	}
+	else {
+		// line is somewhat vertical
+		// make sure y0 < y1
+		if (y0 > y1) {
+			std::swap (y0, y1);
+			std::swap (x0, x1);
+		}
+
+		float a = dx / dy;
+		float x = x0;
+
+		for (int y = y0; y <= y1; ++y) {
+			image.set (x, y, color);
+			x += a;
+		}
+	}
+
 }
 
 int main (int argc, char** argv)
 {
 	TGAImage image (500, 500, TGAImage::RGB);
 
-	//line (13, 20, 80, 40, image, white);
 	draw_line (-50, -200, 60, 240, image, white);
 	image.flip_vertically ();
 
