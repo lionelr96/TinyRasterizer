2c215d6ad19ebc8ce5141e1a9c860a3afdfcfbd1
LIONEL ROBERT <lionelr@umich.edu>
2022-08-14 21:04:08 -0400

    able to (somewhat) render 3D cubes

--- TinyRasterizer/geometry.h
+++ TinyRasterizer/geometry.h
@@ -2,6 +2,8 @@
 
 #include <ostream>
 #include <cmath>
+#include <array>
+#include "tgaimage.h"
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -32,6 +34,7 @@ template <class t> struct Vec3 {
 	inline Vec3<t> operator -(const Vec3<t>& v) const { return Vec3<t> (x - v.x, y - v.y, z - v.z); }
 	inline Vec3<t> operator *(float f)          const { return Vec3<t> (x * f, y * f, z * f); }
 	inline t       operator *(const Vec3<t>& v) const { return x * v.x + y * v.y + z * v.z; }
+	inline t operator[] (const int n) const { return n == 0 ? x : (n == 1 ? y : z); }
 	float norm () const { return std::sqrt (x * x + y * y + z * z); }
 	Vec3<t>& normalize (t l = 1) { *this = (*this) * (l / norm ()); return *this; }
 	template <class > friend std::ostream& operator<<(std::ostream& s, Vec3<t>& v);
@@ -46,12 +49,37 @@ template <class t> struct Point {
 	Point (t _x, t _y) : x (_x), y (_y) {}
 };
 
+template <class t> struct Point3 {
+	union {
+		struct { t x, y, z; };
+		t raw[3];
+	};
+	Point3 () : x (0), y (0), z (0) {}
+	Point3 (t _x, t _y, t _z) : x (_x), y (_y), z (_z) {}
+};
+
 typedef Vec2<float> Vec2f;
 typedef Vec2<int>   Vec2i;
 typedef Vec3<float> Vec3f;
 typedef Vec3<int>   Vec3i;
 typedef Point<float> Pointf;
 typedef Point<int> Pointi;
+typedef Point3<int> Point3i;
+typedef Point3<float> Point3f;
+
+class Triangle {
+public:
+	TGAColor color;
+	std::array<Vec3f, 3> vertices;
+public:
+
+	Triangle (Vec3f _x, Vec3f _y, Vec3f _z, TGAColor c) : color (c) {
+		vertices[0] = _x;
+		vertices[1] = _y;
+		vertices[2] = _z;
+	}
+};
+
 
 template <class t> std::ostream& operator<<(std::ostream& s, Vec2<t>& v) {
 	s << "(" << v.x << ", " << v.y << ")\n";
--- TinyRasterizer/main.cpp
+++ TinyRasterizer/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <array>
 #include "geometry.h"
 #include "tgaimage.h"
 #include "model.h"
@@ -7,6 +8,11 @@
 const TGAColor white = TGAColor (255, 255, 255, 255);
 const TGAColor red = TGAColor (255, 0, 0, 255);
 const TGAColor green = TGAColor (0, 255, 0, 255);
+const TGAColor yellow = TGAColor (255, 255, 0, 255);
+const TGAColor blue = TGAColor (0, 0, 255, 255);
+const TGAColor purple = TGAColor (159, 43, 104, 255);
+const TGAColor cyan = TGAColor (0, 255, 255, 255);
+
 Model* model = NULL;
 const int width = 1000;
 const int height = 1000;
@@ -64,6 +70,10 @@ void draw_line (int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 
 }
 
+std::pair<int, int> convert_2D_points_to_coordinates (Pointi p) {
+	return std::make_pair (p.x, p.y);
+}
+
 void draw_wireframe_triangle (Pointi p0, Pointi p1, Pointi p2, TGAImage& image, TGAColor color) {
 
 	// draws triangles based on points
@@ -115,22 +125,140 @@ void draw_filled_triangle (Pointi p0, Pointi p1, Pointi p2, TGAImage& image, TGA
 		}
 	}
 
+}
+
+std::pair<float, float> viewport_to_canvas (float x, float y) {
+	return std::make_pair (x * 1000 / width, y * 1000 / height);
+}
+
+Pointi project_vertex (Vec3f& v, int d) {
+	float x = (v[0] * d) / v[2];
+	float y = (v[1] * d) / v[2];
+	std::pair<float, float> point = viewport_to_canvas (x, y);
+	Pointi p (point.first, point.second);
+	return p;
+}
+
+void render_triangle (Triangle& triangle, std::vector<Pointi>& projected, TGAImage& image) {
+	Vec3f vertex0 = triangle.vertices[0];
+	Vec3f vertex1 = triangle.vertices[1];
+	Vec3f vertex2 = triangle.vertices[2];
+	Pointi point0 = project_vertex (vertex0, 200);
+	Pointi point1 = project_vertex (vertex1, 200);
+	Pointi point2 = project_vertex (vertex2, 200);
+
+	draw_wireframe_triangle (point0, point1, point2, image, triangle.color);
+}
+
+void render_object (std::vector<Vec3f>& vertices, std::vector<Triangle>& triangles, TGAImage& image, int d) {
+	std::vector<Pointi> projected{};
+	for (int i = 0; i < vertices.size (); ++i) {
+		projected.push_back (project_vertex (vertices[i], d));
+	}
+
+	for (int i = 0; i < triangles.size (); ++i) {
+		render_triangle (triangles[i], projected, image);
+	}
 
 }
 
 int main (int argc, char** argv)
 {
 	TGAImage image (width, height, TGAImage::RGB);
+	int distance = 200;
 
-	Pointi p0 (-200 + 300, -250 + 300);
+	/*Pointi p0 (-200 + 300, -250 + 300);
 	Pointi p1 (200 + 300, 50 + 300);
-	Pointi p2 (20 + 300, 250 + 300);
+	Pointi p2 (20 + 300, 250 + 300);*/
 
-	draw_filled_triangle (p0, p1, p2, image, green); // drawing triangle
-	draw_wireframe_triangle (p0, p1, p2, image, white); // coloring in triangle
+	//draw_filled_triangle (p0, p1, p2, image, green); // drawing triangle
+	//draw_wireframe_triangle (p0, p1, p2, image, white); // coloring in triangle
+
+	// the four "front" vertices
+	//Vec3f vAf (-1 + 2, 1 + 2, 1);
+	//Vec3f vBf (1 + 2, 1 + 2, 1);
+	//Vec3f vCf (1 + 2, -1 + 2, 1);
+	//Vec3f vDf (-1 + 2, -1 + 2, 1);
+
+	//// the four "back" vertices
+	//Vec3f vAb (-1 + 2, 1 + 2, 1.2);
+	//Vec3f vBb (1 + 2, 1 + 2, 1.2);
+	//Vec3f vCb (1 + 2, -1 + 2, 1.2);
+	//Vec3f vDb (-1 + 2, -1 + 2, 1.2);
+
+	//// the front face
+	//Pointi point0 = project_vertex (vAf, distance);
+	//Pointi point1 = project_vertex (vBf, distance);
+	//Pointi point2 = project_vertex (vCf, distance);
+	//Pointi point3 = project_vertex (vDf, distance);
+	//std::pair<float, float> coordinate0 = convert_2D_points_to_coordinates (point0);
+	//std::pair<float, float> coordinate1 = convert_2D_points_to_coordinates (point1);
+	//std::pair<float, float> coordinate2 = convert_2D_points_to_coordinates (point2);
+	//std::pair<float, float> coordinate3 = convert_2D_points_to_coordinates (point3);
+	//draw_line (coordinate0.first, coordinate0.second, coordinate1.first, coordinate1.second, image, blue);
+	//draw_line (coordinate1.first, coordinate1.second, coordinate2.first, coordinate2.second, image, blue);
+	//draw_line (coordinate2.first, coordinate2.second, coordinate3.first, coordinate3.second, image, blue);
+	//draw_line (coordinate3.first, coordinate3.second, coordinate0.first, coordinate0.second, image, blue);
+
+	//// the back face
+	//point0 = project_vertex (vAb, distance);
+	//point1 = project_vertex (vBb, distance);
+	//point2 = project_vertex (vCb, distance);
+	//point3 = project_vertex (vDb, distance);
+	//std::pair<float, float> coordinate4 = convert_2D_points_to_coordinates (point0);
+	//std::pair<float, float> coordinate5 = convert_2D_points_to_coordinates (point1);
+	//std::pair<float, float> coordinate6 = convert_2D_points_to_coordinates (point2);
+	//std::pair<float, float> coordinate7 = convert_2D_points_to_coordinates (point3);
+	//draw_line (coordinate4.first, coordinate4.second, coordinate5.first, coordinate5.second, image, red);
+	//draw_line (coordinate5.first, coordinate5.second, coordinate6.first, coordinate6.second, image, red);
+	//draw_line (coordinate6.first, coordinate6.second, coordinate7.first, coordinate7.second, image, red);
+	//draw_line (coordinate7.first, coordinate7.second, coordinate4.first, coordinate4.second, image, red);
+
+	//// the front-to-back edges
+	//draw_line (coordinate0.first, coordinate0.second, coordinate4.first, coordinate4.second, image, green);
+	//draw_line (coordinate1.first, coordinate1.second, coordinate5.first, coordinate5.second, image, green);
+	//draw_line (coordinate2.first, coordinate2.second, coordinate6.first, coordinate6.second, image, green);
+	//draw_line (coordinate3.first, coordinate3.second, coordinate7.first, coordinate7.second, image, green);
+
+	// vertices
+	Vec3f v0 (1 + 2, 1 + 2, 1 + 2);
+	Vec3f v1 (-1 + 2, 1 + 2, 1 + 2);
+	Vec3f v2 (-1 + 2, -1 + 2, 1 + 2);
+	Vec3f v3 (1 + 2, -1 + 2, 1 + 2);
+	Vec3f v4 (1 + 2, 1 + 2, -1 + 2);
+	Vec3f v5 (-1 + 2, 1 + 2, -1 + 2);
+	Vec3f v6 (-1 + 2, -1 + 2, -1 + 2);
+	Vec3f v7 (1 + 2, -1 + 2, -1 + 2);
+
+	std::vector<Vec3f> vertices;
+	vertices.push_back (v0);
+	vertices.push_back (v1);
+	vertices.push_back (v2);
+	vertices.push_back (v3);
+	vertices.push_back (v4);
+	vertices.push_back (v5);
+	vertices.push_back (v6);
+	vertices.push_back (v7);
+
+	std::vector<Triangle> triangles;
+	triangles.push_back (Triangle (vertices[0], vertices[1], vertices[2], red));
+	triangles.push_back (Triangle (vertices[0], vertices[2], vertices[3], red));
+	triangles.push_back (Triangle (vertices[4], vertices[0], vertices[3], green));
+	triangles.push_back (Triangle (vertices[4], vertices[3], vertices[7], green));
+	triangles.push_back (Triangle (vertices[5], vertices[4], vertices[7], blue));
+	triangles.push_back (Triangle (vertices[5], vertices[7], vertices[6], blue));
+	triangles.push_back (Triangle (vertices[1], vertices[5], vertices[6], yellow));
+	triangles.push_back (Triangle (vertices[1], vertices[6], vertices[2], yellow));
+	triangles.push_back (Triangle (vertices[4], vertices[5], vertices[1], purple));
+	triangles.push_back (Triangle (vertices[4], vertices[1], vertices[0], purple));
+	triangles.push_back (Triangle (vertices[2], vertices[6], vertices[7], cyan));
+	triangles.push_back (Triangle (vertices[2], vertices[7], vertices[3], cyan));
+
+	render_object (vertices, triangles, image, distance);
+
 
 	image.flip_vertically ();
-	image.write_tga_file ("output_filled_triangle.tga");
+	image.write_tga_file ("output_perspective_projection.tga");
 	delete model;
 
 	return 0;
